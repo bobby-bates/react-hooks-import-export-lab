a5d951a090ed7a4acb8a12d0dffa0c2c76567638
Bobby Bates <robertfbatesjr@gmail.com>
2022-05-29 21:34:19 -0400

    completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,16 @@
 import React from "react";
+import NavBar from './NavBar'
+import Home from './Home'
+import About from './About'
+import { username, city, image } from '../data/user'
 
 function App() {
   return (
-    <div>
+    <>
       <NavBar />
-      <Home />
-      <About />
-    </div>
+      <Home username={username} city={city} />
+      <About image={image} />
+    </>
   );
 }
 
